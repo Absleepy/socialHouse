97125237a6e198d324c64030e09b10ce4f9a0698
Absleepy <now.abidali@gmail.com>
2021-09-04 00:38:13 +0500

    about page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import Footer from "./Components/CommonComponents/Footer/Footer";
 import Home from "./Pages/Home/Index";
 import "./style/App.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./Pages/Home/About/Index";
 import Header from "./Components/CommonComponents/Header/Header";
 function App() {
   return (
     <div className="wrapper">
-      <Router>
+      <Router basename="/">
         <Header />
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/about" exact>
-            <About />
-          </Route>
-        </Switch>
+        <div className="page_renderer">
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about" exact>
+              <About />
+            </Route>
+          </Switch>
+        </div>
       </Router>
+
       <Footer />
     </div>
   );
