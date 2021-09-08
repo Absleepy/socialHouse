783bcac63d14ba4b4f19acf3823d7d5f3189f810
Absleepy <now.abidali@gmail.com>
2021-09-09 00:29:04 +0500

    privacy policy

--- src/Components/PageComponents/PrivacyPolicy/Hero/Hero.jsx
+++ src/Components/PageComponents/PrivacyPolicy/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import styles from "./Hero.module.scss";
 const Hero = () => {
   return (
     <div className={styles.container}>
-      <h1 className="text-white text-center font-weight-700">Contact Us</h1>
+      <h1 className="text-white text-center font-weight-700">PRIVACY POLICY</h1>
     </div>
   );
 };
